b56c0b1870169cf6833792d2fbb69913f7743ef5
Bader Obad <baderobad02@gmail.com>
2024-11-01 19:56:48 +0200

    first commit

---
+++ Assets/Scripts/MTable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    void Start()
+    {
+    int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+int S = 5; 
+for (int Y = 1; Y <= 10; Y++)
+{
+    int result = Multiply(S,Y);
+    Debug.Log($"{S} x {Y} = {result}");
+    //Debug.Log($"{S} x {Y} = {result}");
+}
+
+
+    }
+
+    
+        
+    
+}
